1ee28e8f1266f7efc575e2e6a2ae07809e300c08
ichigo48 <buckinghampalace48@gmail.com>
2022-08-22 02:23:10 +0900

    記事編集画面

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1 class="title">編集画面</h1>
+        <div class="content">
+            <form action='/posts/ {{ $post->id }}' method='POST'>
+                @csrf
+                @method('PUT')
+                <div class="title">
+                    <h2>Title</h2>
+                    <input type="text" name ="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
+                <p class="titke_error" style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea type="text" name="post[body]" placeholder="今日も1日お疲れ様でした。">{{ $post->body }}</textarea>
+                <p class="bidy_error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="update"/>
+        </form>
+        <div class="back">[<a href="/posts/{{ $post->id }}">back</a>]</div>
+    </body>
+</html>
